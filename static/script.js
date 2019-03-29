49f344eccca8108e43d7d50de312f53b868b123a
Anton <antonstalhandske@gmail.com>
2019-03-29 12:49:26 +0100

    Fixed duplicate error

--- static/script.js
+++ static/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function() {
     // Kod inne i denna funktionen körs när sidan har laddats klart
 
@@ -16,23 +15,4 @@ $(document).ready(function() {
     $("article.foldable header").on("click", function() {
         $(this).nextAll("*").slideToggle();
     });
-=======
-$(document).ready(function() {
-    // Kod inne i denna funktionen körs när sidan har laddats klart
-
-    /*
-        Döljer alla element (utom "<header>") i alla element av typen
-        "<article>" med klassen "foldable"
-    */
-    $("article.foldable > *:not(header)").hide();
-
-    /*
-        När man klickar på ett "<header>"-element i ett "<article>"-
-        element med klassen "foldable" så döljs/visas alla element
-        som ligger efter "<header>"-elementet som användaren klickade på
-    */
-    $("article.foldable header").on("click", function() {
-        $(this).nextAll("*").slideToggle();
-    });
->>>>>>> 5a09a212bb6bc058dfc2cb7dd84495983dff50a8
 });
